3e9a9cd7b9a9ced625921584508de8217467bb07
Ricard Romeo Murgo <ricard.romeo@immfly.com>
2016-10-12 20:26:05 +0200

    Created iOS project

---
+++ Swift-C-GPS/Swift-C-GPS.h
@@ -0,0 +1,19 @@
+//
+//  Swift-C-GPS.h
+//  Swift-C-GPS
+//
+//  Created by Ricard Romeo Murgo on 12/10/16.
+//  Copyright © 2016 Ricard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swift-C-GPS.
+FOUNDATION_EXPORT double Swift_C_GPSVersionNumber;
+
+//! Project version string for Swift-C-GPS.
+FOUNDATION_EXPORT const unsigned char Swift_C_GPSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_C_GPS/PublicHeader.h>
+
+
---
+++ Swift-C-GPSTests/Swift_C_GPSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_C_GPSTests.swift
+//  Swift-C-GPSTests
+//
+//  Created by Ricard Romeo Murgo on 12/10/16.
+//  Copyright © 2016 Ricard. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_C_GPS
+
+class Swift_C_GPSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
